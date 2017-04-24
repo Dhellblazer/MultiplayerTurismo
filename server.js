35edf0289c88035dbdc1f80ee4bc68ebafa6c95e
Dhellblazer <diegoanb107@gmail.com>
2017-04-24 12:15:38 -0500

    Archivos Originales
    Estos son los archivos originales, donde crea servidor, y recibe los jugadores, aún no elimina los inactivos.

---
+++ server.js
@@ -0,0 +1,38 @@
+//----------------Funciona Perfectamente
+var socket = require('socket.io');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+var players = [];
+
+function Player (id) {
+    this.id = id;
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+    this.entity = null;
+}
+
+io.sockets.on('connection', function(socket) {
+    socket.on ('initialize', function () {
+            var idNum = players.length;
+            var newPlayer = new Player (idNum);
+            players.push (newPlayer);
+
+            socket.emit ('playerData', {id: idNum, players: players});
+            socket.broadcast.emit ('playerJoined', newPlayer);
+    });
+
+    socket.on ('positionUpdate', function (data) {
+            players[data.id].x = data.x;
+            players[data.id].y = data.y;
+            players[data.id].z = data.z;
+
+        socket.broadcast.emit ('playerMoved', data);
+    });
+});
+
+console.log ('Server started.');
+server.listen(process.env.PORT || 3000);
---
+++ subirArchivos.bat
@@ -0,0 +1,4 @@
+git add .
+git status
+git commit -am "make it better"
+git push heroku master
